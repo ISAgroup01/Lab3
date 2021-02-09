12b909e89ca90d5594c6d5bb8e5300578a535ddf
Group01 <s270450@studenti.polito.it>
2021-02-09 20:23:09 +0100

    Added control unit, working in progress

--- src/alu.vhd
+++ src/alu.vhd
@@ -6,7 +6,7 @@ use WORK.alu_types.all;
 
 entity ALU is
   generic (N : integer := 32);
-  port 	 ( FUNC: IN TYPE_OP;
+  port 	 ( FUNC: IN aluOp;
            DATA1, DATA2: IN std_logic_vector(N-1 downto 0);
            OUTALU: OUT std_logic_vector(N-1 downto 0));
 end ALU;
@@ -20,10 +20,19 @@ P_ALU: process (FUNC, DATA1, DATA2)
 
   begin
     case FUNC is
-	when ADD      =>  OUTALU <= std_logic_vector(unsigned(DATA1) + unsigned(DATA2)) ; 
-	when SUB      =>  OUTALU <= std_logic_vector(unsigned(DATA1) - unsigned(DATA2)) ;
+	when ADD      => OUTALU <= std_logic_vector(signed(DATA1) + signed(DATA2)) ; 
+        when EXOR     => OUTALU <= DATA1 xor DATA2;
+        when SLT      =>;
+        when ADDI     => OUTALU <= std_logic_vector(signed(DATA1) + signed(DATA2));
+        when SRAI     => OUTALU <= std_logic_vector(shift_right(signed(DATA1), to_integer(unsigned(DATA2)))); -- arithmetic shift right
+        when ANDI     =>;
+        when BEQ      =>;
+        when LW       =>;
+        when SW       =>;
+        when JAL      =>;
+        when AUIPC    =>;
+        when LUI      =>;
 	when BITAND   =>  OUTALU <= DATA1 and DATA2;
-	when BITXOR   =>  OUTALU <= DATA1 xor DATA2;
 	when FUNCASR  =>  OUTALU <= std_logic_vector(shift_right(signed(DATA1), to_integer(unsigned(DATA2)))); -- arithmetic shift right
                                                               
 	when others => null;
